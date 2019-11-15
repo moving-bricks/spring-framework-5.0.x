5e52a72df730831349d13684021bb55987895d00
haoxiaoyong1014 <hxyHelloWorld@163.com>
2019-11-15 16:28:03 +0800

    spring-source

---
+++ lubanIoc/src/main/java/com/luban/anno/EanbleLuabn.java
@@ -0,0 +1,14 @@
+package com.luban.anno;
+
+import com.luban.imports.MyImportSelector;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+
+
+public @interface EanbleLuabn {
+}
---
+++ lubanIoc/src/main/java/com/luban/dao/IndexDao3.java
@@ -0,0 +1,28 @@
+package com.luban.dao;
+
+import com.luban.imports.MyInvocationHandler;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Proxy;
+
+
+public class IndexDao3 implements BeanPostProcessor {
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		if (beanName.equals("indexDao")){
+			bean = Proxy.newProxyInstance(this.getClass().getClassLoader(),new Class[]{Dao.class},new MyInvocationHandler(bean));
+
+		}
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		return null;
+	}
+
+
+
+}
